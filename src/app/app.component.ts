494e86bda324a56872e214d4f89d9d17c3c32da9
Kajalwankhede <kajuwankhede1510@gmail.com>
2023-02-14 23:12:28 +0530

    UC 5- To Display Hello from Bridgelabz with user inputted name with name validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl:string ="../assets/logo.png"
   url:string = "https://www.bridgelabz.com";
   userName: string =""; 
+  nameError:string="";
   
 ngOnInit(): void{
    this.title="Hello From BridgeLabz !!!";
@@ -18,5 +19,17 @@ ngOnInit(): void{
 clickEvent(event:any){
   console.log("Clicked on bridgelabz logo! ",event);
   window.open(this.url,"_blank");
+}
+onInput($event:any){
+  console.log("Change Event occured!!",$event.data);
+  const nameRegex=RegExp('^[A-Z][a-z]{2,}$');
+  if(nameRegex.test(this.userName))
+  {
+    this.nameError ="";
+    return;
+  }
+  else{
+    this.nameError="UserName is Incorrect!";
+  }
 }
 }
